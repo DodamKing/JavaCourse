1fa870fd26f3e684d91dfcdffecf69a8fb5b1716
Dodamking <power6120@naver.com>
2021-09-15 00:15:52 +0900

    Sept_14_homework

--- java/works/210914/src/t05_hwk/Hwk_02.java
+++ java/works/210914/src/t05_hwk/Hwk_02.java
@@ -1,12 +1,13 @@
 package t05_hwk;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 // 수를 입력 받아 리스트로 오름차순 정렬 출력
-public class Hwk_01 {
+public class Hwk_02 {
 	public static void main(String[] args) {
-		int number, temp;
+		int number;
 		Scanner scanner = new Scanner(System.in);
 		
 		ArrayList<Integer> vos = new ArrayList<Integer>();
@@ -20,15 +21,8 @@ public class Hwk_01 {
 		
 		System.out.println("원본자료: " + vos);
 		
-		for (int i = 0; i < vos.size(); i++) {
-			for (int j = 0; j < vos.size()-1; j++) {
-				if (vos.get(j) > vos.get(j+1)) {
-					temp = vos.get(j);
-					vos.set(j, vos.get(j+1));
-					vos.set(j+1, temp);
-				}
-			}
-		}
+		Collections.sort(vos);
+		
 		System.out.println("정렬자료: " + vos);
 		
 		scanner.close();
