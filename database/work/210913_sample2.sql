e4ad8f9466702ed143e7df5ed8587682fcfe019a
dodamKing <power6120@naver.com>
2021-09-14 13:07:50 +0900

    Sept_14_checkp

--- database/work/210913_sample2.sql
+++ database/work/210913_sample2.sql
@@ -1,8 +1,8 @@
 show databases;
 show grants for atom2@localhost;
 
-select database(); -- 현재 선택되어 있는 데이터베이스명 보여주기
-select user(); -- 현재 사용중인 계정 보여주기
+select database(); -- �쁽�옱 �꽑�깮�릺�뼱 �엳�뒗 �뜲�씠�꽣踰좎씠�뒪紐� 蹂댁뿬二쇨린
+select user(); -- �쁽�옱 �궗�슜以묒씤 怨꾩젙 蹂댁뿬二쇨린
 
 use sample2;
 
@@ -11,7 +11,7 @@ mid varchar(15) not null,
 pwd varchar(15) not null,
 name varchar(20) not null,
 agt int default 20,
-sex char(2) default '남자',
+sex char(2) default '�궓�옄',
 ipsail datetime default now(),
 address varchar(50)
 );
@@ -26,23 +26,23 @@ select * from sinsang2;
 
 desc sinsang2;
 
-alter table sinsang2 change sex gender char(2) default '여자';
+alter table sinsang2 change sex gender char(2) default '�뿬�옄';
 
 alter table sinsang2 change agt age int default 20;
 
 desc sinsang2;
 
-insert into sinsang2 values('hong','1234','홍길동',25,'남자','1990-10-05','서울');
-insert into sinsang2 values('kim','1234','김말숙',33,'여자','1997-12-3','부산');
-insert into sinsang2 values('lee','1234','이순자',24,'여자','1985-7-25','광주');
-insert into sinsang2 values('lee1','1234','이기자',40,'남자','1999-10-3','울산');
-insert into sinsang2 values('park','1234','박찬호',32,'남자','1980-6-08','대전');
-insert into sinsang2 values('bae','1234','배은숙',19,'여자','1993-11-22','마산');
-insert into sinsang2 values('son','1234','손기정',45,'남자','1987-12-15','제주');
-insert into sinsang2 values('kim1','1234','김영숙',36,'여자','2002-10-5','부천');
-insert into sinsang2 values('kim2','1234','김영철',39,'남자','1988-10-05','인천');
-insert into sinsang2 values('lee2','1234','이겨라',29,'남자','2000-10-09','서울');
-insert into sinsang2 values('son2','1234','손오공',23,'남자','2005-12-15','서울');
+insert into sinsang2 values('hong','1234','�솉湲몃룞',25,'�궓�옄','1990-10-05','�꽌�슱');
+insert into sinsang2 values('kim','1234','源�留먯닕',33,'�뿬�옄','1997-12-3','遺��궛');
+insert into sinsang2 values('lee','1234','�씠�닚�옄',24,'�뿬�옄','1985-7-25','愿묒＜');
+insert into sinsang2 values('lee1','1234','�씠湲곗옄',40,'�궓�옄','1999-10-3','�슱�궛');
+insert into sinsang2 values('park','1234','諛뺤갔�샇',32,'�궓�옄','1980-6-08','���쟾');
+insert into sinsang2 values('bae','1234','諛곗��닕',19,'�뿬�옄','1993-11-22','留덉궛');
+insert into sinsang2 values('son','1234','�넀湲곗젙',45,'�궓�옄','1987-12-15','�젣二�');
+insert into sinsang2 values('kim1','1234','源��쁺�닕',36,'�뿬�옄','2002-10-5','遺�泥�');
+insert into sinsang2 values('kim2','1234','源��쁺泥�',39,'�궓�옄','1988-10-05','�씤泥�');
+insert into sinsang2 values('lee2','1234','�씠寃⑤씪',29,'�궓�옄','2000-10-09','�꽌�슱');
+insert into sinsang2 values('son2','1234','�넀�삤怨�',23,'�궓�옄','2005-12-15','�꽌�슱');
 
 select * from sinsang2;
 
@@ -50,15 +50,15 @@ delete from sinsang2;
 
 select * from sinsang2 where mid='kim';
 
-select * from sinsang2 where gender='남자';
+select * from sinsang2 where gender='�궓�옄';
 
-select * from sinsang2 where address='서울';
+select * from sinsang2 where address='�꽌�슱';
 
-select * from sinsang2 where address='서울' or address='인천';
+select * from sinsang2 where address='�꽌�슱' or address='�씤泥�';
 
-select * from sinsang2 where address in ('서울', '인천');
+select * from sinsang2 where address in ('�꽌�슱', '�씤泥�');
 
- select * from sinsang2 where address='서울' and gender='남자';
+ select * from sinsang2 where address='�꽌�슱' and gender='�궓�옄';
  
  alter table sinsang2 add point int default 1000;
  
@@ -66,50 +66,53 @@ select * from sinsang2 where address in ('서울', '인천');
  
  select * from sinsang2;
  
- update sinsang2 set point=point+100 where gender='여자';
+ update sinsang2 set point=point+100 where gender='�뿬�옄';
  
  select * from sinsang2;
  
- update sinsang2 set point=point+50 where gender='남자' and ag>=35;
+ update sinsang2 set point=point+50 where gender='�궓�옄' and ag>=35;
  
  select * from sinsang2;
  
- update sinsang2 set address='서울' where mid='kim1';
+ update sinsang2 set address='�꽌�슱' where mid='kim1';
  
  select* from sinsang2;
  
- select mid as 아이디, gender as 성별, ipsail as 입사일 from sinsang2;
+ select mid as �븘�씠�뵒, gender as �꽦蹂�, ipsail as �엯�궗�씪 from sinsang2;
  
- insert into sinsang2 (mid, pwd, name, age, gender, address) values ('park', '1234', '박세리', 35, default, '인천');
+ insert into sinsang2 (mid, pwd, name, age, gender, address) values ('park', '1234', '諛뺤꽭由�', 35, default, '�씤泥�');
  
  select * from sinsang2;
  
  select mid, age, gender, address from sinsang2 where age between 30 and 39;
  
- select * from sinsang2 where address!='서울' and address!='인천' and gender='여자';
+ select * from sinsang2 where address!='�꽌�슱' and address!='�씤泥�' and gender='�뿬�옄';
  
- update sinsang2 set point=point+500 where address!='서울' and address!='인천' and gender='여자';
+ update sinsang2 set point=point+500 where address!='�꽌�슱' and address!='�씤泥�' and gender='�뿬�옄';
  
- select * from sinsang2 where gender='여자';
+ select * from sinsang2 where gender='�뿬�옄';
  
- select * from sinsang2 where gender='남자' and ipsail<'1990-1-1';
+ select * from sinsang2 where gender='�궓�옄' and ipsail<'1990-1-1';
  
- update sinsang2 set point=0 where gender='남자' and ipsail<'1990-1-1';
- 
- select * from sinsang2;
- 
- update sinsang2 set gender='여자' where point=0 and age>=40;
- 
- select * from sinsang2 where address='서울';
- 
- update sinsang2 set point=point+300 where address='서울';
- 
- update sinsang2 set pwd='0000' where address='서울';
+ update sinsang2 set point=0 where gender='�궓�옄' and ipsail<'1990-1-1';
  
  select * from sinsang2;
  
- select * from sinsang2 where point=0 and gender='남자' and ipsail<'1985-1-1';
+ update sinsang2 set gender='�뿬�옄' where point=0 and age>=40;
  
- delete from sinsang2 where point=0 and gender='남자' and ipsail<'1985-1-1';
+ select * from sinsang2 where address='�꽌�슱';
+ 
+ update sinsang2 set point=point+300 where address='�꽌�슱';
+ 
+ update sinsang2 set pwd='0000' where address='�꽌�슱';
  
  select * from sinsang2;
+ 
+ select * from sinsang2 where point=0 and gender='�궓�옄' and ipsail<'1985-1-1';
+ 
+ delete from sinsang2 where point=0 and gender='�궓�옄' and ipsail<'1985-1-1';
+ 
+ select * from sinsang2;
+ 
+ insert into sinsang2 (mid, pwd, name, age, gender, address) values ('ddd', '1234', '디디디', 31, '남자', '청주');
+ 
--- java/works/210913/src/t01_sql/test1.sql
+++ java/works/210913/src/t01_sql/test1.sql
@@ -82,12 +82,12 @@ select truncate(avg(age),0) from sinsang2;
 select gender as 성별, ceil(avg(point)) as 평균 from sinsang2 group by gender;
 
 -- floor 정수내림
-select gender as 성별, floor(avg(point)) as 평균 from sinsang2 group by gender;
-select gender, floor(avg(age)) from sinsang2 group by gender;
+select gender as 성별, floor(avg(point)) as 평균 from sinsang2 group by gender; /* 주석이 왜 안 먹지*/
+select gender, floor(avg(age)) from sinsang2 group by gender; /* 잘 먹네 */
 
 -- abs 절대값
 select abs(-500); /* 주석 잘 되는데 왜 */
-select abs((avg(age) group by gender='여자') - (avg(age) group by gender='남자')); /* 안되는듯 */
+select abs((avg(age) group by gender='여자') - (avg(age) group by gender='남자'));  /* 안되는듯 */
 
 -- mod 나머지
 select floor(10/3) as 몫, mod(10,3) as 나머지;
@@ -109,6 +109,134 @@ select lower('SEOUL');
 
 -- concat(문자열1, 문자열2)
 select concat(name, '의 나이는 ', age, '살 입니다.') from sinsang2;
+
+-- 문자열 반환: substr(string, position, len)
+select substr('Welcome to Korea', 3, 7); /* 시작 위치가 0이 아니네 */
+select substr('안녕 세상', 2, 3);
+select substr('951225-1234567', 8, 1) as 성별번호;
+select substr('951225-1234567', 1, 6) as 생년월일;
+select concat(substr('951225-1234567', 1, 2), '년 ', substr('951225-1234567', 3, 2), '월 ', substr('951225-1234567', 5, 2), '일') as 생년월일;
+
+-- 문자열 인덱스로 substring_index(문자열, 추출기준, 개수)
+select substring_index('사과, 배, 바나나, 포도, 수박, 딸기', ',', 3); /* 개수를 음수로 표현하면 뒤에서 부터 */
+select substring_index('사과, 배, 바나나, 포도, 수박, 딸기', ',', -4);
+select substring_index(substring_index('사과, 배, 바나나, 포도, 수박, 딸기', ',', 4), ',', -1);
+select substring_index(substring_index('사과, 배, 바나나, 포도, 수박, 딸기', ',', -3), ',', 1);
+
+
+-- 디비에 있는 생년월일과 시간 출력
+insert into sinsang2 values ('gid', '1234', '고인돌', default, default, default, '청주', default);
+select name, ipsail from sinsang2;
+select name, concat(substr(ipsail, 1, 4), '년 ', substr(ipsail, 6, 2), '월 ', substr(ipsail, 9, 2), '일 ', substr(ipsail, 12, 2), '시 ', substr(ipsail, 15, 2), '분') from sinsang2 where ipsail>'2021-1-1';
+select name, ipsail, substr(ipsail, 1) from sinsang2 where name='고인돌';
+select concat('성: ', substr(name,1,1),  ', 이름: ', substr(name,2,2)) from sinsang2;
+
+-- 문자열 추출 : mid(문자열, 시작위치, 출력할크기)
+select name, ipsail, mid(ipsail, 6, 2) from sinsang2;
+select name, ipsail, left(ipsail, 10) from sinsang2; /* 왼쪽부터 */
+select name, ipsail, right(ipsail, 8) from sinsang2; /* 오른쪽부터 */
+
+-- 문자 존재 유무 instr(문자열, '검색문자')
+select * from sinsang2;
+select name, instr(name, '길') from sinsang2; /* 위치를 출력 없으면 0*/
+insert into sinsang2 values ('msa', '1234', '못 살아', default, default, default, '청 주', default);
+select name, address, instr(name, ' '), instr(address, ' ') from sinsang2;
+
+-- 문자열 치환 replace(문자열, 찾을문자, 바꿀문자)
+select name, replace(name, '홍', '가') from sinsang2;
+select name, replace(name, ' ', '') from sinsang2;
+select address, replace(address, ' ', '') from sinsang2;
+
+-- 날짜 함수
+select now(); /* 현재 날짜 시간  출력 */
+select year(now()); /* 현재 시스템의 날짜중 연도만 */
+select month(now());
+select day(now());
+select date(now());
+select time(now());
+
+select concat(substr(date(now()), 1, 4), '년 ', substr(date(now()), 6, 2), '월 ', substr(date(now()), 9, 2), '일');
+select concat(name, '회원의 입사일은 ', substr(ipsail, 1, 4), '년 ', substr(ipsail, 6, 2), '월 ', substr(ipsail, 9, 2), '일 입니다.') from sinsang2 where name='홍길동';
+select concat(name, '회원의 입사일은 ', year(ipsail), '년 ', month(ipsail), '월 ', day(ipsail), '일 입니다.') from sinsang2 where name='홍길동';
+
+-- 시간 함수
+select hour(now());
+select minute(now());
+select second(now());
+
+select concat('현재 시간은 ', hour(now()), '시 ', minute(now()), '분 ', second(now()), '초 입니다.');
+
+-- 요일 함수
+select week(now()); /* 1년 기준 몇주차인지 */
+select dayname(now()); /* 요일 출력 */
+select monthname(now()); /* 월 이름으로 출력 */
+
+/* 날짜 형식 지정 date_format(날짜, 포맷) 
+ * %Y(4자리), %y(2자리), %M(월 이름), %m(월을 2자리로 출력), %c(월을 한자리로 출력), %d, $e(일을 한자리로 출력)
+ * %W(요일이름), %a(요일약자), %b(월약자)
+ * %H(24시), %l(12시), %i(분), %s(초)
+ */
+select * from sinsang2;
+
+select date_format(now(), '%Y-%m-%d');
+select date_format(ipsail, '%Y-%c-%d') from sinsang2;
+select date_format(ipsail, '%y-%c-%d') from sinsang2;
+select date_format(ipsail, '%y-%c-%e') from sinsang2;
+select date_format(ipsail, '%Y-%M-%d') from sinsang2;
+select date_format(ipsail, '%Y-%m-%d') from sinsang2;
+select date_format(ipsail, '%Y-%M-%d %W') from sinsang2;
+select date_format(ipsail, '%Y-%b-%d %a') from sinsang2;
+
+select date_format(ipsail, '%H-%i-%s') from sinsang2 where name='소나무';
+select date_format(ipsail, '%h-%i-%s') from sinsang2 where name='소나무';
+select date_format(now(), '%H-%i_%s');
+select date_format(now(), '%h-%i_%s');
+
+select date_format(now(), '%h시 %i분 %s초');
+
+-- SELECT date, SUBSTR( _UTF8'일월화수목금토', DAYOFWEEK(date), 1) AS week FROM 테이블; dayofweek하면 요일이 숫자로 나옴 일요일이 1임
+select name, concat(substr('일월화수목금토', dayofweek(ipsail), 1), '요일') as 요일 from sinsang2;
+
+-- 현재 날짜 출력 sysdate(), current_timestamp()
+select now(), sysdate(), current_timestamp();
+
+-- 날짜 연산 date_add() interval 명령과 같이 사용
+select now(), date_add(now(), interval 1 day);
+select now(), date_add(now(), interval 1 hour);
+select now(), date_add(now(), interval 30 minute);
+select now(), date_add(now(), interval 1 second);
+select now(), date_add(now(), interval -1 day);
+select now(), date_add(now(), interval 1 year);
+select now(), date_add(now(), interval 1 month);
+select now(), date_add(now(), interval 24 hour);
+select now(), date_add(now(), interval 30 day);
+
+-- 달의 마지막 일자 last_day(날짜형식)
+select last_day(now());
+select last_day('2021-2-1');
+select last_day('2020-2-2');
+
+-- 날짜 차이 계산 to_days()
+select now(), now() - 1;
+select now(), to_days(now()) - to_days('2021-9-1');
+select to_days('2021-1-31') - to_days('2021-1-1');
+
+-- 날짜 비교 datediff()
+select datediff(now(), '2021-9-1');
+
+select timestampdiff(quarter, '2021-1-1', now()); /* 분기 */
+
+-- 천단위 마다 ',' 표시 format(수, 소수점 이하 자리) 
+select format(1234556, 0);
+select format(1234556.123, 0);
+select format(1234556.456789, 2);
+
+
+
+
+
+
+
 
 
 
